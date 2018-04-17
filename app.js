a2203eca2e51796708c3d2b2b39e16810aaab689
Filipe Costa <filipe.tcn@gmail.com>
2018-04-17 19:53:39 -0300

    modificacao final

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-var express = require('express');
-var app = express();
+var app = require('./config/express')();
 
-app.set('view engine', 'ejs');
+var roldanaRoutes = require("./app/routes/catalogo")(app);
 
-app.get('/catologo',function(req,res){
-    res.render("roldanas/catalogo");
-});
 app.listen(3000, function(){
     console.log("Roldanos rodando!");
 });
---
+++ app/routes/catalogo.js
@@ -0,0 +1,6 @@
+module.exports = function(app){  
+    app.get('/catalogo',function(req,res){
+      
+    res.render("roldanas/catalogo");    
+});
+}
