df996e1e19269fdf9ba502e4964e0a5b5c8d2b0a
Filipe Costa <filipe.tcn@gmail.com>
2018-04-18 14:09:18 -0300

    Funcionou carai

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 module.exports = function(){
     return mysql.createConnection({
-        host:'roldanainstance.cqunr0spvtoh.us-east-1.rds.amazonaws.com',
+        host:'roldana.c0bkp8fbp76u.us-east-1.rds.amazonaws.com',
         user:'root',
         password:'Pa$$4eit',
         database:'roldanaDB'
